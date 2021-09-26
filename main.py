f7b3a30621bfe9b3560eba987e6a842351d0e608
bobby <bobbyhieubui@gmail.com>
2021-09-26 12:45:41 -0700

    added html component

--- main.py
+++ main.py
@@ -99,4 +99,4 @@ app.jinja_env.globals['csrf_token'] = generate_csrf_token
 app.secret_key = os.environ['SESSION_SECRET_KEY']
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(ssl_context='adhoc', debug=True)
